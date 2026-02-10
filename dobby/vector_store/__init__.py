411daff3a86fd5414f9d4413280c0db034bb82b6
Anant Gupta <anant.gupta@tynybay.com>
2026-02-10 11:12:44 +0530

    chore: remove phantom FinancialDocument import and bump to 0.2.11

--- dobby/vector_store/__init__.py
+++ dobby/vector_store/__init__.py
@@ -1,3 +1,2 @@
 from .azure_ai_search import AzureAISearchVectorStore as AzureAISearchVectorStore
 from .base import Document as Document
-from .financial_document import FinancialDocument as FinancialDocument
